8e6b19ded9e5086acf72bb78794730d6140afbc5
Lindauro D.A. Neto <140420389+LindauroNeto@users.noreply.github.com>
2024-04-20 12:58:16 -0300

    Atualizar o tstGUI.java

--- TestandoCodigos/src/tstGUI.java
+++ TestandoCodigos/src/tstGUI.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class tstGUI {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setTitle("Cocô e Mijo");
+        frame.setTitle("Tela em Branco");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setSize(500,500);
