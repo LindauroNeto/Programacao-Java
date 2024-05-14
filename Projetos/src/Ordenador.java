a2e1278b21870450a729f5104cf10daac361cf7a
LindauroNeto <140420389+LindauroNeto@users.noreply.github.com>
2024-05-14 20:31:32 -0300

    14.05 - atualizacao arquivos

---
+++ Projetos/src/Ordenador.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+public class Ordenador {
+    static int[] listNumbers = new int[10];
+    static Random random = new Random();
+
+    static void ordenador(){
+        // *- O código deste método não é meu. Vi pelo canal 'Coding with John'
+        for (int i = 1; i < listNumbers.length; i++) {
+            int maiorValor = listNumbers[i];
+
+            int j = i - 1;
+            while (j >= 0 && listNumbers[j] > maiorValor) {
+                listNumbers[j + 1] = listNumbers[j];
+                j--;
+            }
+            listNumbers[j + 1] = maiorValor;
+        }
+        // *-
+
+        System.out.print("[ ");
+        for (int i = 0; i < listNumbers.length; i++) {
+            System.out.print(listNumbers[i]+ " ");
+        }
+        System.out.print("]");
+    }
+    
+    public static void main(String[] args) {
+        System.out.print("[ ");
+        for (int i = 0; i < listNumbers.length; i++) {
+            listNumbers[i] = random.nextInt(100+1);
+            System.out.print(listNumbers[i]+ " ");
+        }
+        System.out.println("]");
+        
+        ordenador();
+    }
+}
