46033e8a1028321a9bb357dfbf8f115cb065f20c
LindauroNeto <140420389+LindauroNeto@users.noreply.github.com>
2024-05-15 01:19:32 -0300

    15.05
    Novo projeto de um gerador de senha

---
+++ Projetos/src/senha/CriadorDeSenha.java
@@ -0,0 +1,20 @@
+package senha;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CriadorDeSenha {
+	static Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.println("Quantos digitos você vai querer a sua senha?");
+		try {
+			int leitor = scanner.nextInt();
+			PasswordCreator.geradorSenha(leitor);
+		} catch (InputMismatchException e) {
+			System.out.println("Valor inválido.");
+		} finally {
+			scanner.close();
+		}
+	}
+
+}
---
+++ Projetos/src/senha/PasswordCreator.java
@@ -0,0 +1,37 @@
+package senha;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PasswordCreator {
+	static ArrayList<String> organizador = new ArrayList<String>();
+	static Random aleatorio = new Random();
+	static char ranNumber;
+	// v Insipirado pela Amannitta!!! Não foi feito originalmente por mim!!!!! Confira o repositório dela: https://github.com/Amannitta/Gerador-de-Senhas
+	static String opcoesGeral = "abcçdefghijklmnopqrstuvwxyzABCÇDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$_%&?*§=+¢/\\";
+	static String opcoesLetra = "abcçdefghijklmnopqrstuvwxyzABCÇDEFGHIJKLMNOPQRSTUVWXYZ";
+	static String opcoesMais = "0123456789!@#$_%&?*§=+¢/\\";
+	
+	public static void geradorSenha(int qnt) {
+		if (qnt > 5) {
+			for (int i = 0; i < qnt; i++) {
+				ranNumber = opcoesLetra.charAt(aleatorio.nextInt(opcoesLetra.length()));
+				organizador.add(Character.toString(ranNumber));
+			}
+			
+			for (int i = 0; i <= qnt / 3; i++) {
+				ranNumber = opcoesMais.charAt(aleatorio.nextInt(opcoesMais.length()));
+				organizador.set(aleatorio.nextInt(organizador.size() - 1), Character.toString(ranNumber));
+			}
+		} else {
+			for (int i = 0; i < qnt; i++) {
+				ranNumber = opcoesGeral.charAt(aleatorio.nextInt(opcoesGeral.length()));
+				organizador.add(Character.toString(ranNumber));
+			}
+		}
+		
+		for (int i = 0; i < organizador.size(); i++) {
+			System.out.print(organizador.get(i));
+		}
+	}
+}
