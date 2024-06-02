bb5bf430c2dc2dab36d2428492df881e80ffe13c
LindauroNeto <140420389+LindauroNeto@users.noreply.github.com>
2024-06-02 00:31:24 -0300

    01.06 v4

--- TestandoCodigos/src/TestandoIterators.java
+++ TestandoCodigos/src/TestandoIterators.java
@@ -11,13 +11,25 @@ public class TestandoIterators {
 		nomes.add("Matheus");
 		nomes.add("Amanda");
 		
+		// Coleta o Iterador da lista
 		Iterator<String> it = nomes.iterator();
 
-//		System.out.println(it.next());
+		// Imprime o próximo item da lista (no caso, aqui vai imprimir o primeiro item dela, "João")
+		//System.out.println(it.next());
 		
+		// Printando todos os itens
+		// "Se tiver um 'Próximo item', então:"
 		while (it.hasNext()) {
-			System.out.println("* " + it.next());
+			String i = it.next();
+			System.out.println("* " + i);
 		}
+		
+		// Outro caso de printar todos os itens: (Não vale a pena)
+//		System.out.println(it.next());
+//		System.out.println(it.next());
+//		System.out.println(it.next());
+//		System.out.println(it.next());
+		// Bateu o limite do iterador, além disso, o programa vai dar erro.
 		
 		System.out.println();
 		
@@ -26,7 +38,10 @@ public class TestandoIterators {
 			numeros.add(i);
 		}
 		
+		// Coletando o iterador da lista 'numeros'
 		Iterator<Integer> itn = numeros.iterator();
+		
+		// "Se 'próximo item' for 'true' (no caso, se ainda existir um elemento depois), então:"
 		while (itn.hasNext()) {
 			Integer i = itn.next();
 			System.out.println("i: " + i);
