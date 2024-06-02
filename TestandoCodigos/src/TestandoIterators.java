26cc2d1e758c8e4186fb548801cefa2de679765b
LindauroNeto <140420389+LindauroNeto@users.noreply.github.com>
2024-06-02 00:01:46 -0300

    01.06 v2

--- TestandoCodigos/src/TestandoIterators.java
+++ TestandoCodigos/src/TestandoIterators.java
@@ -1,9 +1,40 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class TestandoIterators {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<String> nomes = new ArrayList<String>();
+		
+		nomes.add("João");
+		nomes.add("Maria");
+		nomes.add("Matheus");
+		nomes.add("Amanda");
+		
+		Iterator<String> it = nomes.iterator();
 
+//		System.out.println(it.next());
+		
+		while (it.hasNext()) {
+			System.out.println("* " + it.next());
+		}
+		
+		System.out.println();
+		
+		ArrayList<Integer> numeros = new ArrayList<Integer>();
+		for (int i = 0; i <= 10; i++) {
+			numeros.add(i);
+		}
+		
+		Iterator<Integer> itn = numeros.iterator();
+		while (itn.hasNext()) {
+			Integer i = itn.next();
+			System.out.println("i: " + i);
+			if (i % 2 != 0) {
+				itn.remove();
+			}
+		}
+		System.out.println(numeros);
 	}
 
 }
